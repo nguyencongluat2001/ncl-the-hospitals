b5dd2a339fe6729950ba01c19b52573f22c431f4
Luật Nguyễn <luatnc@efy.com.vn>
2024-09-25 10:28:02 +0700

    f

--- modules/Client/Page/Home/Controllers/HomeController.php
+++ modules/Client/Page/Home/Controllers/HomeController.php
@@ -348,8 +348,7 @@ class HomeController extends Controller
         $fileName = 'FILE_KET_QUA.pdf';
         $fileName = Library::_replaceBadChar($fileName);
         $fileName = Library::_convertVNtoEN($fileName);
-        // $sFullFileName =  'file-kq/'.$input['id'] . "!~!" . $fileName;
-        $sFullFileName =  $input['id'] . "!~!" . $fileName;
+        $sFullFileName =  'file-kq/'.$input['id'] . "!~!" . $fileName;
         $pdf->save($sFullFileName);
         return array(
             'success' => true,
